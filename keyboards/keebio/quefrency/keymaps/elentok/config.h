e593692512450a9067876a10a85638bc572c5da3
Dudi Elentok <dudi.elentok@salto.io>
2023-09-06 14:57:51 +0300

    Quefrency: more improvements

--- keyboards/keebio/quefrency/keymaps/elentok/config.h
+++ keyboards/keebio/quefrency/keymaps/elentok/config.h
@@ -23,4 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 
-#define TAPPING_TERM 150
+// #define TAPPING_TERM 150
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+// #define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
